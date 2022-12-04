4ec6cff9de72330d35ffb6d049f1d19284d1a219
stevebiczyk <stevebiczyk@gmail.com>
2022-12-04 16:45:48 +0000

    Create and display game board

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+from random import randint
+
+board = []
+
+def create_board(board):
+    for x in range(7):
+        board.append(["O"] * 7)
+
+
+def print_board(board):
+    for row in board:
+        print((" ").join(row))
+
+
+print("Let's play Battleship!")
+create_board(board)
+print_board(board)
