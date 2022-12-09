b430557360f715826e827a4c973ae894769a6222
stevebiczyk <stevebiczyk@gmail.com>
2022-12-09 12:45:49 +0000

    Added computer targeting function

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def welcome_msg():
     print("Your goal is to beat the Computer.")
     print("You have 5 ships each, whoever sinks the opponent's ship is the winner!")
     print("Make your selection by choosing a number between 1 and 6")
-    print("The board is 6 columns and 6 rows. Hits are marked with @ and misses are X")
+    print("The board is 6 columns and 6 rows. Hits are marked with * and misses are X")
 
 
 def create_board(board):
@@ -56,6 +56,7 @@ def place_ships(board):
     """
     Randomly generate and place 5 ships on the board.
     A counter keeps track of the number of ships.
+    Duplicate ship placement is also handled by the counter.
     """
     for ship in range(5):
         ship_row, ship_column = randint(0, len(board) - 1), randint(0, len(board) - 1)
@@ -64,25 +65,65 @@ def place_ships(board):
         board[ship_row][ship_column] = "S"
 
 
-#def player_target():
+def computer_target(board):
+
+    comp_row, comp_column = randint(0, len(board) - 1), randint(0, len(board) - 1)
+    if (PLAYER_BOARD[comp_row][comp_column] == "*" or
+            PLAYER_BOARD[comp_row][comp_column] == "X"):
+        comp_row = randint(0, len(board) - 1)
+        comp_column = randint(0, len(board) - 1)
+    elif PLAYER_BOARD[comp_row][comp_column] == "S":
+        print("Player, your battleship has been hit!")
+        print(
+            f"The computer hit row {comp_row}"
+            f" and column {comp_column}")
+        PLAYER_BOARD[comp_row][comp_column] = "*"
+        computer_score += 1
+    else:
+        print(
+            f"The computer hit row {comp_row}"
+            f" and column {comp_column}")
+        print("You're lucky player, the computer missed!")
+        PLAYER_BOARD[comp_row][comp_column] = "X"
 
 
+#def player_target_position():
+    """
+    Player target selection
+    
+    turns = 0
+    print("Chose your target location!")
+    while turns <= 10:
+        try:
+            target_row = int(input("Select the row you want to target: \n"))
+        except:
+     """       
+
+
+#validate_player_target():
 #def computer_target_():
+#def ships_sunk():
+
 
 welcome_msg()
 create_board(PLAYER_BOARD)
 place_ships(PLAYER_BOARD)
 print_board(PLAYER_BOARD)
 print("This is the player's board")
+create_board(HIDDEN_BOARD)
+place_ships(HIDDEN_BOARD)
 create_board(COMPUTER_BOARD)
-place_ships(COMPUTER_BOARD)
 print_board(COMPUTER_BOARD)
 print("This is the computer's board")
 print("The ships have been placed, let the game begin!")
+computer_target(PLAYER_BOARD)
 
+
+# Remaining functions/features to be added
 
 # Place ships on the board
 # Player selection for placing the shot
 # Random selection for computer's shot
 # Check how many ships are hit
+# Score counter
 # Round counter
